91774ee425359f2143f7991c18d90ce3d9b4bc78
giver-kdk <giverbro13@gmail.com>
2023-07-17 17:41:55 +0545

    day2 done

---
+++ day02/day2.js
@@ -0,0 +1,16 @@
+// Boolean Toogle Logic using if-else
+let toggle1 = false;
+
+if(toggle1 == true)
+{
+	toggle1 = false;
+} 
+else toggle1 = true;
+console.log(toggle1);
+
+
+// Boolean Toogle Logic using NOT operator
+let toggle = false;
+
+toggle = !toggle;
+console.log(toggle);
