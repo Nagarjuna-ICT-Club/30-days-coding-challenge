2fbb56861637b228e241de7faf5365a03d37e23c
giver-kdk <giverbro13@gmail.com>
2023-07-24 21:07:22 +0545

    day 9 updated

--- day09/day9.js
+++ day09/day9.js
@@ -1,6 +1,6 @@
 let array1 = [1, 2, 3, 4, 5];
 // Copy "array1" into "array2"
-let array2 = Array.from(array5);
+let array2 = Array.from(array1);
 
 let object1 = 
 {
@@ -9,3 +9,5 @@ let object1 =
 };
 // Copy "object1" into "object2"
 let object2 = Object.assign({}, object1);
+// console.log(array2);
+// console.log(object2);
