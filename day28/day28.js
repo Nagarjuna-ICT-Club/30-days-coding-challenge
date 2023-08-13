48e2ca247a8f3e5958fa893aab162eaa5f09f2b5
giver-kdk <giverbro13@gmail.com>
2023-08-13 19:17:54 +0545

    day 28

--- day28/day28.js
+++ day28/day28.js
@@ -6,7 +6,7 @@ let granny = document.querySelector(".granny");
 let parent = document.querySelector(".parent");
 let child = document.querySelector(".child");
 
-// Event Bubbling
-granny.addEventListener("click", handleOne, false);
-parent.addEventListener("click", handleTwo, false);
-child.addEventListener("click", handleThree, false);
+// Event Capturing
+granny.addEventListener("click", handleOne, true);
+parent.addEventListener("click", handleTwo, true);
+child.addEventListener("click", handleThree, true);
